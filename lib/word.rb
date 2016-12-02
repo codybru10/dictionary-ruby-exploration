758f4cd262067905b56c7f662bb8d235fab57615
Cody <cody.brubaker@gmail.com>
2016-12-01 22:26:33 -0800

    complete unit testing for Word class

--- lib/word.rb
+++ lib/word.rb
@@ -1,11 +1,37 @@
 class Word
 
   attr_reader(:word)
+  @@word_array = []
 
   define_method(:initialize) do |attribute|
     @word = attribute.fetch(:word)
+    @id = @@word_array.length().+(1)
   end
 
+  define_singleton_method(:all) do
+    @@word_array
+  end
 
+  define_method(:save) do
+    @@word_array.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@word_array = []
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@word_array.each() do |word|
+      if word.id.eql?(id)
+        found_word = word
+      end
+    end
+    found_word
+  end
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,13 +2,58 @@ require('rspec')
 require('word')
 
 describe(Word) do
+  before() do
+  Word.clear()
+end
 
-  describe(:word) do
+  describe('#word') do
     it('returns the word entered') do
     test_word = Word.new(:word => "hello")
     expect(test_word.word()).to(eq("hello"))
     end
   end
+
+  describe('.all') do
+    it('returns an empty array') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('add a word to the word array') do
+      test_word = Word.new(:word => "hello")
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears word array') do
+      test_word = Word.new(:word => "hello").save()
+      test_word.clear()
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#id') do
+    it('create a unique ID for each word') do
+      test_word = Word.new(:word => "hello")
+      expect(test_word.id()).to(eq(1))
+    end
+  end
+
+  describe('.find') do
+    it('finds a words unique ID in an array') do
+      test_word = Word.new(:word => "hello")
+      test_word.save()
+      test_word2 = Word.new(:word => "hello")
+      test_word2.save()
+      expect(Word.find(test_word.id())).to(eq(test_word))
+
+    end
+  end
+
+
 
 
 end
