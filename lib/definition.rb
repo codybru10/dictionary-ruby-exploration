d0fc00400abc40a71cc3cd68be6c9cb3f78de2cf
Cody Brubaker <cody.brubaker@gmail.com>
2016-12-02 09:41:23 -0800

    add unit specs for definition class

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,13 +1,42 @@
 class Definition
 
-
+  @@definition_array = []
 
   define_method(:initialize) do |definition|
     @definition = definition
+    @id = @@definition_array.length().+(1)
   end
 
   define_method(:definition) do
     @definition
   end
+
+  define_singleton_method(:all) do
+    @@definition_array
+  end
+
+  define_method(:save) do
+    @@definition_array.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@definition_array = []
+  end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:find) do |id|
+    found_definition = nil
+    @@definition_array.each() do |definition|
+      if definition.id().eql?(id)
+        found_definition = definition
+      end
+    end
+    found_definition
+  end
+
+
 
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('definition')
 
 describe(Definition) do
+  before() do
+    Definition.clear()
+  end
 
   describe('#definition') do
     it('returns the definition of a word') do
@@ -9,6 +12,46 @@ describe(Definition) do
       expect(test_definition.definition()).to(eq('a greeting'))
     end
   end
+
+  describe('.all') do
+    it('returns an empty array') do
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves definition to array') do
+      test_definition = Definition.new("a greeting")
+      test_definition.save()
+      expect(Definition.all()).to(eq([test_definition]))
+    end
+  end
+
+  describe('.clear') do
+    it('clears definition array') do
+      test_definition = Definition.new("a greeting").save()
+      test_definition.clear()
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+
+  describe('#id') do
+    it('assigns an ID to a definition') do
+      test_definition = Definition.new("a greeting")
+      expect(test_definition.id()).to(eq(1))
+    end
+  end
+
+  describe('.find') do
+    it('finds a definition by its unique ID') do
+      test_definition = Definition.new("a greeting")
+      test_definition.save()
+      test_definition2 = Definition.new("a goodbye")
+      test_definition2.save()
+      expect(Definition.find(test_definition.id())).to(eq(test_definition))
+    end
+  end
+
 
 
 end
