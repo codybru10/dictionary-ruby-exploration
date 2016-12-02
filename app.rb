75dfe798a57bc5e6990f35d8b6a03cb0be6a6f75
Cody Brubaker <cody.brubaker@gmail.com>
2016-12-02 11:38:38 -0800

    allow user add definition to word, but adds definition to all words

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word')
+require('./lib/definition')
 
 get('/') do
   @words_array = Word.all
@@ -21,9 +22,21 @@ end
 
 get('/words/:id') do
   @word = Word.find(params.fetch('id').to_i())
+  @definition = Definition.find(params.fetch('id').to_i())
+  @definition_array = Definition.all()
   erb(:word)
 end
 
 get('/words/:id/definitions/new') do
+  @word = Word.find(params.fetch('id').to_i())
   erb(:definition_form)
+end
+
+post('/definitions') do
+  @description= params.fetch('definition')
+  @definition = Definition.new(@description)
+  @definition.save()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@defintion)
+  erb(:success2)
 end
