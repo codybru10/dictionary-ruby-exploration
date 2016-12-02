6871ae9cebcc0d2acb988cc9e9b7ff766a232306
Cody Brubaker <cody.brubaker@gmail.com>
2016-12-02 08:47:04 -0800

    add second integration spec for completing form

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -10,3 +10,12 @@ describe('the path to add word form', {:type => :feature}) do
     expect(page).to have_content('Add Word to Dictionary')
   end
 end
+
+describe('the path to the success page after completing the add word form', {:type => :feature}) do
+  it('processes the users input and takes them to the success page') do
+    visit('/words/new')
+    fill_in('word', :with => 'hello')
+    click_button('Add Word')
+    expect(page).to have_content('Word has been added to dictionary')
+  end
+end
